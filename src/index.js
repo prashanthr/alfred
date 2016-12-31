39a0164aaa836496422ef03f01b8463e30466a13
Prashanth Rajaram <prashanthrdev@gmail.com>
2016-12-31 12:34:34 -0800

    Adding debug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import _debug from 'debug'
 var express = require('express')
 var app = express()
+var debug = _debug('alfred-server')
 
 app.get('/',function(req,res) {
   res.send("Hello Alfred!")
