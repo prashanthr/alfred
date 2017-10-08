1e4e1ec67157850342a8a9ad0d7c8135bc4fb02f
Prashanth <prashanthrdev@gmail.com>
2017-10-08 11:53:41 -0700

    Stash

--- bin/update-config.js
+++ bin/update-config.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import { has, keys, forEach, flatten, map } from 'lodash'
 import _debug from 'debug'
 var debug = _debug('update-config')
 
@@ -11,7 +12,31 @@ async function updateConfig () {
   const config = JSON.parse(configFile)
   debug('Config', JSON.stringify(config))
   const secretFiles = fs.readdirSync(path.join(__dirname, '../config/secrets'))
+  debug('secretFiles', secretFiles)
+  // const search = (obj, searchKey, result) => {
+  //   debug(`Searching ${JSON.stringify(obj)} for ${searchKey}. Result ${result}`)
+  //   if (has(obj, searchKey)) {
+  //     result.push(searchKey)
+  //     return result
+  //   } else {
+  //     if (typeof obj === 'object') {
+  //       keys(obj).forEach(key => {
+  //         result.push(key)
+  //         if (typeof obj[key] === 'object') {
+  //           search(obj[key], searchKey, result)
+  //         }
+  //       })
+  //     } else {
+  //       result = []
+  //     }
+  //   }
+  // }
+
   secretFiles.forEach(file => {
+    const key = file.replace('.json', '')
+    const obj = search(config, key, [])
+    debug(`Obj ${JSON.stringify(obj)}`)
+    configFile.x(file)
     // _.find(modules, _.flow(
     //   _.property('submodules'),
     //   _.partialRight(_.some, { id: 2 })
