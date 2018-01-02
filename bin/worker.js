34ca944fa33a311308d2e90d8a7ac4822804412d
Prashanth <prashanthrdev@gmail.com>
2018-01-01 21:02:11 -0800

    fix worker

--- bin/worker.js
+++ bin/worker.js
@@ -6,42 +6,63 @@ import getTaskWorker from '../src/job/get-task-worker'
 
 const debug = _debug('worker')
 
-const listen = (callback) => {
-  var listener = async () => {
-    debug('here')
-    var message = await axios.get(`${config.apiBaseUrl}/jobs/next`)
-    debug('msg', message)
-    if (message.data) {
-      callback(message.data, listener)
-    } else {
-      setTimeout(listener, 20000)
-    }
-  }
-  return {
-    on: () => null,
-    start: () => setTimeout(listener, 0)
-  }
-}
+// const listen = (callback) => {
+//   var listener = async () => {
+//     debug('here')
+//     var message = await axios.get(`${config.apiBaseUrl}/jobs/next`)
+//     debug('msg', message)
+//     if (message.data) {
+//       callback(message.data, listener)
+//     } else {
+//       setTimeout(listener, 20000)
+//     }
+//   }
+//   return {
+//     on: () => null,
+//     start: async () => setTimeout(listener, 0)
+//   }
+// }
 
-async function work () {
+// async function work () {
+//   debug(`Processing job...`)
+//   const processingFunction = async (message, next) => {
+//     try {
+//       const worker = getTaskWorker(message.type)
+//       debug('Retrieved worker', worker)
+//       await worker.start()
+//       debug('Done.')
+//       next()
+//     } catch (err) {
+//       debug('Error while performing job', err)
+//       next()
+//     }
+//   }
+//   const workman = listen(processingFunction)
+//   debug('workman', workman)
+//   workman.start()
+// }
+
+async function work2 () {
   debug(`Processing job...`)
-  const processingFunction = async (message, next) => {
+  const job = async () => {
     try {
+      const response = await axios.get(`${config.apiBaseUri}/jobs/next`)
+      const message = response.data
+      if (!message) {
+        return
+      }
+      debug('Message', message)
       const worker = getTaskWorker(message.type)
       debug('Retrieved worker', worker)
       await worker.start()
       debug('Done.')
-      next()
     } catch (err) {
       debug('Error while performing job', err)
-      next()
     }
   }
-  const workman = listen(processingFunction)
-  debug('workman', workman)
-  workman.start()
+  setTimeout(job, 0)
 }
 
-work().catch(err => {
+work2().catch(err => {
   debug(`Error in worker process`, err)
 })
